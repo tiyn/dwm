9e0818634f71a9ffc094d7542698bb1c3a95fb50
TiynGER <kantemarten@googlemail.com>
2020-03-29 00:31:40 +0100

    new border

--- config.def.h
+++ config.def.h
@@ -18,7 +18,7 @@ static const char col_cyan[]        = "#ffa500";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3 },
 };
 
 /* tagging */
