685c69599ec27619534f57504ec58e264b9eee18
TiynGER <kantemarten@googlemail.com>
2020-03-31 10:12:04 +0200

    new config for new patch

--- config.def.h
+++ config.def.h
@@ -36,10 +36,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class							instance							title       tags mask     isfloating   monitor */
-	{ "Gimp",							NULL,								NULL,       0,            1,           -1 },
-	{ "Firefox",						NULL,								NULL,       1 << 8,       0,           -1 },
-	{ NULL,								"de.uol.swp.client.ClientApp",		NULL,       0,            1,           -1 },
+	/* class							instance							title       tags mask     isCentered	 isfloating   monitor */
+	{ "Gimp",							NULL,								NULL,       0,            0,             1,           -1 },
+	{ "Firefox",						NULL,								NULL,       1 << 8,       0,			 0,           -1 },
+	{ NULL,								"de.uol.swp.client.ClientApp",		NULL,       0,            0,			 1,           -1 },
 };
 
 /* layout(s) */
