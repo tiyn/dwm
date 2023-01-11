c6a2e45ba3e340fc44345d60b466baf6c55d870f
tiyn <tiyn@mail-mk.eu>
2023-01-11 01:40:33 +0100

    cleanup: unused programs removed

--- config.def.h
+++ config.def.h
@@ -32,7 +32,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class    instance                        title       tags mask     iscentered    isfloating   monitor */
-	{ NULL,	    "origin.exe",                   NULL,       0,            0,	    1,           -1 },
 };
 
 /* layout(s) */
