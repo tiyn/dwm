5eb0da07cfdd99cec4d678938f9215367e592621
tiyn <tiyn@mail-mk.eu>
2023-10-15 05:54:48 +0200

    updated config

--- config.def.h
+++ config.def.h
@@ -128,4 +128,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
