008c92268a9910f81ae87d7adba7a0a0da58764b
tiyn <tiyn@mail-mk.eu>
2023-10-25 04:31:06 +0200

    changed icons

--- config.def.h
+++ config.def.h
@@ -36,7 +36,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
